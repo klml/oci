68a497ed012122cffd38189008828ca0b5c778dd
Klaus Mueller <m@klml.de>
2020-06-29 09:57:38 +0200

    Create oci.sh

---
+++ oci.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# [openshift cli](https://docs.openshift.com/container-platform/latest/cli_reference/openshift_cli/developer-cli-commands.html) with a little bit more idempotence
+case "$1" in
+"expose" )
+    oc get routes $3 2> /dev/null || oc $@ ;;
+* ) ## pipe all other oc commands to oc 
+    oc $@ ;;
+esac
