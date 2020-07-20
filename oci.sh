fa0be1248d164c2e2ee46e8101013ddc3ae9aaa5
Klaus Mueller <m@klml.de>
2020-07-20 17:26:13 +0200

    + new-app, + new-build

--- oci.sh
+++ oci.sh
@@ -3,6 +3,10 @@
 case "$1" in
 "expose" )
     oc get routes $3 2> /dev/null || oc $@ ;;
+"new-app" )
+    oc get services $3 2> /dev/null || oc $@ ;;
+"new-build" )
+    oc get build $3 2> /dev/null || oc $@ ;;
 * ) ## pipe all other oc commands to oc 
     oc $@ ;;
 esac
